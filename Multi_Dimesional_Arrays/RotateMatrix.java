a22b0c890b66c9ff516f848eecfd20e1e53a65d2
ramu siripalli <ramsiripalli2772@gmail.com>
2023-05-11 14:35:49 +0530

    RotateMatrix.java
    added geeks for geeks link

--- Multi_Dimesional_Arrays/RotateMatrix.java
+++ Multi_Dimesional_Arrays/RotateMatrix.java
@@ -3,7 +3,7 @@ Problem Description
 You are given an n x n 2D matrix A representing an image. Rotate the image by 90 degrees (clockwise).
 You need to do this in place.
 Note: If you end up using an additional array, you will only receive partial score.
-
+https://www.geeksforgeeks.org/rotate-a-matrix-by-90-degree-in-clockwise-direction-without-using-any-extra-space/
  */
 
 public class RotateMatrix {

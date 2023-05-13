ecce1f3bc369179f1e8543f37abb150cead805d1
ramu siripalli <ramsiripalli2772@gmail.com>
2023-05-13 11:29:30 +0530

    Sorting Noble Integer
    Given an array arr[], find a Noble integer in it. An integer x is said to be Noble in arr[] if the number of integers greater than x is equal to x. If there are many Noble integers, return any of them. If there is no, then return -1

---
+++ Sorting/NobleInteger.java
@@ -0,0 +1,36 @@
+
+  import java.util.Arrays;
+    /*
+    Problem Description
+    Given an integer array A, find if an integer p exists in the array
+    such that the number of integers greater than p in the array equals p.
+
+    https://www.geeksforgeeks.org/noble-integers-in-an-array-count-of-greater-elements-is-equal-to-value/
+     */
+    public class NobleInteger {
+        public static int nobleInteger(int[] Array)
+        {
+            int size = Array.length;  //use Array.length to calculate length of the Array
+            Arrays.sort(Array);  // use Arrays.sort for sorting which takes O(nlogn) time complexity
+            for(int i=0;i<size;i++)
+            {
+                //The continue statement breaks one iteration (in the loop),
+                // if a specified condition occurs, and continues with the next iteration in the loop.
+                if(Array[i] == Array[i+1])
+                    continue;
+                if(Array[i] == (size-i-1)) // (size-i-1) means check
+                    // where the same elements are present after sorting
+                {
+                    System.out.println("Noble Integer Exists");
+                    return 1;
+                }
+            }
+            return -1;
+        }
+        public static void main(String[] args) {
+            int A[] = new int[]{4,1,1,1};
+           nobleInteger(A);
+        }
+    }
+
+
